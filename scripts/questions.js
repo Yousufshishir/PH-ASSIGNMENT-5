f3b4e5a9087633534f6f1260f6495ce39ad91052
Md. Yousuf <yousufbinshahin1@gmail.com>
2025-03-02 10:51:11 +0000

    15

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,9 +1,11 @@
 document.getElementById("back-to-desk-button").style.display="none"
 document.getElementById("show-hero").style.display="none"
 
-document.getElementById("question-tasks").addEventListener("click",function(){
+document.getElementById("question-tasks").addEventListener("click",function(event){
+    event.preventDefault();
 
 document.getElementById("nav-right-hide").style.display="none"
+document.getElementById("blogs").style.display="block"
 document.getElementById("back-to-desk-button").style.display="block"
 
 
@@ -15,7 +17,8 @@ document.getElementById("show-hero").style.display="block"
 })
 
 
-document.getElementById("back-to-desk-button").addEventListener("click",function(){
+document.getElementById("back-to-desk-button").addEventListener("click",function(event){
+    event.preventDefault();
 
 window.location.href="./index.html"
 
